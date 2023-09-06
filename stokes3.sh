09f5b2d443cd4aa56a881ebc9b68748f13663818
Ugo PELISSIER <ugo.pelissier@etu.minesparis.psl.eu>
2023-09-06 14:04:18 +0200

    ✅ Add CLI arguments

--- stokes3.sh
+++ stokes3.sh
@@ -4,7 +4,22 @@ clear
 python="/mnt/host/c/Anaconda3/python.exe"
 freefem="/mnt/host/c/Users/ugo.pelissier/FreeFem++/FreeFem++.exe"
 
+if [ "$1" == "-h" ]; then
+    echo "Usage: ./stokes2.sh -l <l> -c <c>"
+    echo "l: mesh size of the lines"
+    echo "c: mesh size of the circles"
+    exit 0
+fi
+
+while getopts l:c: flag
+do
+    case "${flag}" in
+        l) l=${OPTARG};;
+        c) c=${OPTARG};;
+    esac
+done
+
 cd stokes3
-$python geo.py
+$python geo.py -l $l -c $c
 bash ./mesh.sh
 $freefem stokes3.edp
