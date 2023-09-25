46b715d9ea6c1d1a2ca05a76295028fd5012950e
UgoPelissier <upelissier@gmail.com>
2023-09-25 08:07:38 +0000

    🚧 Update 2d script

--- stokes2.sh
+++ stokes2.sh
@@ -2,37 +2,29 @@
 clear
 
 python="/mnt/host/c/Anaconda3/python.exe"
+gmsh="/mnt/host/c/Users/ugo.pelissier/gmsh/gmsh.exe"
 freefem="/mnt/host/c/Users/ugo.pelissier/FreeFem++/FreeFem++.exe"
 
 if [ "$1" == "-h" ]; then
-    echo "Usage: ./stokes2.sh -p <p> -n <n> -rmin <rmin> -rmax <rmax> -L_ref <L_ref> -H_ref <H_ref> -m <m> -l <l> -c <c>"
-    echo "p: maximum number of holes in the domain"
-    echo "n: number of files to create for each number of holes"
-    echo "rmin: minimum radius"
-    echo "rmax: maximum radius"
-    echo "L_ref: reference length"
-    echo "H_ref: reference height"
-    echo "m: margin between the holes and the domain borders"
+    echo "Usage: ./stokes3.sh -p <p> -n <n> -l <l> -c <c>"
+    echo "p: number of holes in the domain"
+    echo "n: number of files to generate"
     echo "l: mesh size of the lines"
     echo "c: mesh size of the circles"
     exit 0
 fi
 
-while getopts p:n:rmin:rmax:L_ref:H_ref:m:l:c: flag
+while getopts p:n:l:c: flag
 do
     case "${flag}" in
         p) p=${OPTARG};;
-        n) n${OPTARG};;
-        rmin) rmin=${OPTARG};;
-        rmax) rmax=${OPTARG};;
-        L_ref) L_ref=${OPTARG};;
-        H_ref) H_ref=${OPTARG};;
-        m) m=${OPTARG};;
+        n) n=${OPTARG};;
         l) l=${OPTARG};;
         c) c=${OPTARG};;
     esac
 done
 
 cd stokes2
-$python main.py -p $p -n $n -r $rmin $rmax -L_ref $L_ref -H_ref $H_ref -m $m -l $l -c $c
-$freefem stokes.edp
+$python main.py -p $p -n $n -l $l -c $c
+$gmsh convert.geo
+$freefem stokes2.edp
